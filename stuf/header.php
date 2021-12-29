461694e84ee34002f214d6767cc6156a85cb4964
Harshit Mishra <harshitlove28@outlook.com>
2021-12-29 19:15:26 +0530

    creating products initial design

--- stuf/header.php
+++ stuf/header.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Pushster&display=swap" rel="stylesheet">
   <style>
     .main-nav{
-      background-color: rgba(0, 255, 255, 0.404);
+      background-color: rgba(220, 236, 236, 0.404);
       position: sticky;
       top: 0;
       backdrop-filter: blur(10px);
@@ -20,23 +20,43 @@
       font-family:cursive;
       font-weight: 600;
     }
+    .payal{
+      font-family: cursive;
+    }
+    .pp{
+      color: rgb(13, 88, 88);
+    }
   </style>
 
 </head>
 <body>
  
+  <div class="main-nav">
+  <div class=" h-4 p-5 pb-7 flex justify-between border-b-2 border-gray-300 ">
 
-  <div class="main-nav h-4 p-5 pb-7 flex justify-between border-b-2 border-gray-300 ">
 
-    <div class="flex justify-between space-x-3 flex-center items-center">
+    <div class="flex items-center sm:hidden ">
+    <a href="/index.php">  <p class="text-sm pp cursor-pointer"> <span class="text-2xl font-bold payal">P</span>ayal <span class="text-2xl font-bold payal">C</span>omputer's</p> </a>
+
+    </div>
+    <div class="hidden sm:flex justify-between space-x-3 flex-center items-center">
+      <a href="/index.php"> <p class="text-sm pp cursor-pointer"> <span class="text-2xl font-bold payal">P</span>ayal <span class="text-2xl font-bold payal">C</span>omputer's</p>  </a>
         <a href="#" class="text-blue-900 text-sm ai">Home</a>
-        <a href="#" class="text-blue-900 text-sm ai">Products</a>
+        <a href="../products.php" class="text-blue-900 text-sm ai">Products</a>
         <a href="#" class="text-blue-900 text-sm ai">About</a>
     </div>
 
-        <form action="#" method="post" class="flex justify-between space-x-3 flex-center items-center">
-          <input type="text" placeholder="Search.." class="rounded-lg px-2 py-1 sm:w-full ">
+        <form action="#" method="post" class="hidden sm:flex justify-between space-x-3 flex-center items-center">
+          <input type="text" placeholder="Search.." class="rounded-lg px-2 py-1 sm:w-full border-b-2 border-gray-400 ">
           <button type="submit">Search</button>
+        </form>
+
+        <form action="#" method="post" class="flex sm:hidden justify-between space-x-3 flex-center items-center">
+          <input type="text" placeholder="Search.." class="rounded-lg px-2 py-1 sm:w-full border-b-2 border-gray-400">
+          <button type="submit" class="text-blue-700">
+
+            <svg class="w-6 h-6 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
+          </button>
         </form>
     
     <div class="hidden sm:flex justify-around space-x-3 items-center">
@@ -61,10 +81,13 @@
     </div>
 
     <div class="hidden sm:hidden menu-items ">
-        <a href="#" class="block text-center">login</a>
-        <a href="" class="block bg-yellow-300 text-yellow-900 p-2 rounded py-1 hover:bg-yellow-200 text-center" >SignUp</a>
+        <a href="#" class="block text-center">Home</a>
+        <a href="../products.php" class="block text-center">Products</a>
+        <a href="#" class="block text-center">About</a>
+        <a href="" class=" block bg-purple-300 text-purple-900 p-2 rounded py-1 hover:bg-purple-200 ai text-center" >SignUp</a>
       
     </div>
+  </div>
   <!-- header -->
   <script>
     let btn=document.querySelector('button.btn')
